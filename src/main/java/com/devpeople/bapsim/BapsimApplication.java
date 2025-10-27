0eb4a4b8ad4c51a0b322c715a22ea638ca859cc9
Ayoon-b <wnr0303@gmail.com>
2025-10-27 15:57:08 +0900

    initial commit

---
+++ src/main/java/com/devpeople/bapsim/BapsimApplication.java
@@ -0,0 +1,13 @@
+package com.devpeople.bapsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BapsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BapsimApplication.class, args);
+	}
+
+}
