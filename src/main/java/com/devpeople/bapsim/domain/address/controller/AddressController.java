67fc407e197769309c4715c802fd6f368bb5a8ed
Ayoon-b <wnr0303@gmail.com>
2025-11-10 16:46:45 +0900

    Feat: "주소 관련 Repository, Service, Controller 추가"

---
+++ src/main/java/com/devpeople/bapsim/domain/address/controller/AddressController.java
@@ -0,0 +1,21 @@
+package com.devpeople.bapsim.domain.address.controller;
+
+import com.devpeople.bapsim.domain.address.entity.Address;
+import com.devpeople.bapsim.domain.address.service.AddressService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class AddressController {
+    private final AddressService addressService;
+
+    // TODO 조회조건 추가 (ex. 사용자 식별자)
+    @PostMapping("/addressList")
+    public List<Address> getAddressList(){
+        return addressService.getAddressList();
+    }
+}
