0475f8f70994be2a7d80cd1f92c4b809232e1517
junwon_lee <wjdrbs307@naver.com>
2025-11-18 00:42:38 +0900

    장바구니 조회 응답 DTO 작성

--- src/main/java/com/devpeople/bapsim/domain/cart/dto/CartResponseDto.java
+++ src/main/java/com/devpeople/bapsim/domain/cart/dto/CartResponseDto.java
@@ -1,9 +1,34 @@
 package com.devpeople.bapsim.domain.cart.dto;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
 /**
  *  장바구니 조회 응답 DTO
  */
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 public class CartResponseDto {
+
+
+    /** 장바구니 고유 ID */
+    private Long id;
+
+    /** 사용자 ID */
+    private Long userId;
+
+    /** 가게 ID */
+    private Long storeId;
+
+    /** 생성일시 */
+    private LocalDateTime createdAt;
+
+    /** 수정일시 */
+    private LocalDateTime updatedAt;
 }
