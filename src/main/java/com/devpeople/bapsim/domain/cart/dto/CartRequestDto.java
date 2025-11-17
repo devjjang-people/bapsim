079b5afab8adacb623a789f417e09f577e7f7c2a
junwon_lee <wjdrbs307@naver.com>
2025-11-17 23:52:46 +0900

    git test

---
+++ src/main/java/com/devpeople/bapsim/domain/cart/dto/CartRequestDto.java
@@ -0,0 +1,13 @@
+package com.devpeople.bapsim.domain.cart.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 장바구니 생성/수정 요청 DTO
+ */
+
+@Getter
+@Setter
+public class CartRequestDto {
+}
