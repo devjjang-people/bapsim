a6cf5c60c1528dfe61ba40c7c7fb0d8fc0fb7f7d
Ayoon-b <wnr0303@gmail.com>
2025-11-18 00:25:07 +0900

    Feat: "상품 관련 Repository, Service, Controller 추가"

---
+++ src/main/java/com/devpeople/bapsim/domain/product/service/ProductService.java
@@ -0,0 +1,40 @@
+package com.devpeople.bapsim.domain.product.service;
+
+import com.devpeople.bapsim.domain.product.entity.Product;
+import com.devpeople.bapsim.domain.product.repository.ProductRepository;
+import com.devpeople.bapsim.global.exception.ProductNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public Product getProductById(Integer id) {
+
+        return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
+    }
+
+    public List<Product> getProductList() {
+
+        return productRepository.findAll();
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product deleteAddress(Integer id) {
+
+        Product product = productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
+        product.setDescription("상품 삭제");
+        product.setIsDeleted(true);
+
+        return productRepository.save(product);
+    }
+}
