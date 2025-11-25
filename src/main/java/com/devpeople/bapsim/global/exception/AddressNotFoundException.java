f92ca62f14cd9975fa65579b9b92b03fe844b0cc
Ayoon-b <wnr0303@gmail.com>
2025-11-25 22:24:23 +0900

    Feat: "공통 예외 처리 수정, Custom Exception Enum 추가"

--- src/main/java/com/devpeople/bapsim/global/exception/AddressNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.devpeople.bapsim.global.exception;
-
-public class AddressNotFoundException extends RuntimeException {
-    public AddressNotFoundException(Long id) {
-        super("주소를 찾을 수 없습니다. id=" + id);
-    }
-}
--- src/main/java/com/devpeople/bapsim/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/devpeople/bapsim/global/exception/GlobalExceptionHandler.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // 도메인별 커스텀 예외 처리
-    @ExceptionHandler(AddressNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleAddressNotFound(AddressNotFoundException e) {
-        ErrorResponse response = new ErrorResponse("ADDRESS_NOT_FOUND", e.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        ErrorCode code = e.getErrorCode();
+        return ResponseEntity
+                .status(code.getStatus())
+                .body(ErrorResponse.of(code));
     }
 
-    // 그 외 모든 예외 처리 (fallback)
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         ErrorResponse response = new ErrorResponse("INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다.");
