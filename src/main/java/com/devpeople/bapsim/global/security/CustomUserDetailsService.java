b8d5aeeb2449d9cc1e902b37644dbbdb079eadc7
Ayoon-b <wnr0303@gmail.com>
2025-12-16 23:45:33 +0900

    Feat: "Security 설정 추가"

---
+++ src/main/java/com/devpeople/bapsim/global/security/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.devpeople.bapsim.global.security;
+
+import com.devpeople.bapsim.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다. email=" + email));
+    }
+}
